fca50ad1ad3e74eaa277bfed33943087688eb06f
user <abdeldjalilmedjadji907@gmail.com>
2025-09-25 16:22:55 +0100

    jesper

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,11 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { nextjs } from "@next/eslint-config-next";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+/** @type {import('eslint').Linter.Config} */
+export default [
+  ...nextjs,
   {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
+    rules: {
+      "react/no-unescaped-entities": "off",
+    },
   },
 ];
-
-export default eslintConfig;
